df87d008dc8daf79870bc12f0bee905a8c3bebdb
brussia199-ship-it <brussia199@gmail.com>
2026-04-28 13:41:11 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CRYPTOBOT_API_KEY = os.getenv("CRYPTOBOT_API_KEY")
-ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "").split(",") if x]
+ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "7673683792").split(",") if x]
 CRYPTOBOT_API_URL = "https://pay.crypt.bot/api"
