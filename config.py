ecda9b3bc6efd051de9545610ffee4722b2b9626
brussia199-ship-it <brussia199@gmail.com>
2026-04-30 11:50:32 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
-TOKEN = "" # токен бота
+TOKEN = "8027593993:AAFcvWXaW_CetAx7z7s_0iUSFGJsvD5xDbc" # токен бота
 
-ADMIN_ID = 1 # ид админа
+ADMIN_ID = 7673683792 # ид админа
 
-CRYPTOBOT_TOKEN = "" # токен crypto pay 
+CRYPTOBOT_TOKEN = "574332:AAQpkyD58CeMccxcf1fkBHnnKATxQcLSqWy" # токен crypto pay 
 
 CRYPTOBOT_API_URL = 'https://pay.crypt.bot/api/' # ссылка
 
-DB_CHANNEL_ID = "-100" # для хранения товаров, обязательно добавить бота в канал
+DB_CHANNEL_ID = "-1003933759456" # для хранения товаров, обязательно добавить бота в канал
 
 # ссылки 1 - testnet ; 2 - crypto bot
 # 1 - https://testnet-pay.crypt.bot/api/
