cd8cbdc4a99a91ef0fbf7c0987aba4c2b034a673
brussia199-ship-it <brussia199@gmail.com>
2026-04-30 11:49:16 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+TOKEN = "" # токен бота
+
+ADMIN_ID = 1 # ид админа
+
+CRYPTOBOT_TOKEN = "" # токен crypto pay 
+
+CRYPTOBOT_API_URL = 'https://pay.crypt.bot/api/' # ссылка
+
+DB_CHANNEL_ID = "-100" # для хранения товаров, обязательно добавить бота в канал
+
+# ссылки 1 - testnet ; 2 - crypto bot
+# 1 - https://testnet-pay.crypt.bot/api/
+# 2 - https://pay.crypt.bot/api/
