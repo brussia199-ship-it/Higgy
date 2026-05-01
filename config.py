d09c857fde987d9fa43a50e8519274ffdbd7bbbd
brussia199-ship-it <brussia199@gmail.com>
2026-05-01 12:56:12 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+TOKEN = "8787503840:AAF4FSVcF7lpgC93bSL6yWCbgRelDSQh2zM" # токен бота
+
+CHANNEL_ID = "-1003727023817" # ид канала со ставками
+
+ADMIN_CHANNEL_ID = "-1003985525139" # ид канала для заявок (для админа)
+
+ADMIN_ID = 7673683792 # ид админа
+
+CHANNEL_LINK = "https://t.me/UralchikStars" # ссылка на канал со ставками
